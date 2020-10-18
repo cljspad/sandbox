17d07adcefd16caa5b44c79acf2d1a2f9a6ac79c
Thomas Crowley <git@tscrowley.dev>
2020-10-18 13:36:30 +1100

    make shadow-cljs explicit dep

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject cljspad/sandbox "0.1.1"
   :source-paths ["src"]
-  :dependencies [[thheller/shadow-cljs "2.11.4" :scope "provided"]
+  :dependencies [[thheller/shadow-cljs "2.11.4"]
                  [reagent "1.0.0-alpha2"]])
