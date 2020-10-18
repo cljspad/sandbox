faa80a438927356372609329d6075040e0299b03
Thomas Crowley <git@tscrowley.dev>
2020-10-18 13:36:01 +1100

    make reagent explicit dep

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject cljspad/sandbox "0.1.0"
   :source-paths ["src"]
   :dependencies [[thheller/shadow-cljs "2.11.4" :scope "provided"]
-                 [reagent "1.0.0-alpha2" :scope "provided"]])
+                 [reagent "1.0.0-alpha2"]])
